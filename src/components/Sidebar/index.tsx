62d1ae0c8bb23a4064c278c7fd0a0b55eae81e69
viniarifianir16 <viniarifianir16@gmail.com>
2025-02-14 16:41:59 +0700

    feat: update sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -219,17 +219,6 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               Customer
                             </NavLink>
                           </li>
-                          <li>
-                            <NavLink
-                              to="/data/sales"
-                              className={({ isActive }) =>
-                                'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
-                                (isActive && '!text-white')
-                              }
-                            >
-                              Sales
-                            </NavLink>
-                          </li>
                         </ul>
                       </div>
                       {/* <!-- Dropdown Menu End --> */}
